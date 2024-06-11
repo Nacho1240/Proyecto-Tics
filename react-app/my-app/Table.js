68908e24d06832a06206d534a058119c4308caa2
Nacho1240 <damian.elizondo.a@gmail.com>
2024-06-10 23:09:21 -0400

    database

--- react-app/my-app/Table.js
+++ react-app/my-app/Table.js
@@ -1,42 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import { Table, TableWrapper, Row, Rows } from 'react-native-table-component';
+import { View, Text, FlatList } from 'react-native';
 import axios from 'axios';
 
 const TableComponent = () => {
-  const [tableData, setTableData] = useState([]);
-  const [tableHead, setTableHead] = useState(['Column 1', 'Column 2', 'Column 3', 'Column 4']);
+  const [mediciones, setMediciones] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Reemplaza 'https://api.example.com/data' con la URL de tu API
-    axios.get('https://api.example.com/data')
-      .then(response => {
-        const data = response.data;
-        // Transformar datos si es necesario
-        const formattedData = data.map(item => [item.field1, item.field2, item.field3, item.field4]);
-        setTableData(formattedData);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://192.168.1.9:3000/mediciones');
+        console.log('Datos recibidos:', response.data);
+        setMediciones(response.data);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  return (
-    <ScrollView horizontal={true}>
-      <View style={styles.container}>
-        <Table borderStyle={{ borderWidth: 1 }}>
-          <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
-          <Rows data={tableData} textStyle={styles.text}/>
-        </Table>
-      </View>
-    </ScrollView>
+  const renderItem = ({ item }) => (
+    <View style={{ flexDirection: 'column' }}>
+      <Text>Ph:{item.ph}</Text>
+      <Text>{item.microcomponentes.join(', ')}</Text>
+      <Text>{new Date(item.timestamp).toLocaleDateString()}</Text>
+    </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  text: { margin: 6 }
-});
+  if (loading) {
+    return <Text>Cargando...</Text>;
+  }
+
+  if (mediciones.length === 0) {
+    return <Text>No hay datos disponibles</Text>;
+  }
+
+  return (
+    <FlatList
+      data={mediciones}
+      renderItem={renderItem}
+      keyExtractor={(item) => item._id}
+    />
+  );
+};
 
 export default TableComponent;
